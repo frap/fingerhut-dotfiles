7b9e4991f1ebe6870c96e2e1f068da5d7cff4614
Andrés Gasson <gas@red-elvis.net>
2019-12-04 16:27:08 +1300

    added babshka

--- init/40_clj_helpers.sh
+++ init/40_clj_helpers.sh
@@ -1,3 +1,3 @@
 # Install babashka a clojure scripting env
 e_header "Installing babashka - a graalvm based clj"
-sudo su -c bash <(curl -s https://raw.githubusercontent.com/borkdude/babashka/master/install)'
+sudo su -c 'bash <(curl -s https://raw.githubusercontent.com/borkdude/babashka/master/install)'
