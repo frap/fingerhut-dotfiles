a8447700fe8e3f3bb77e63941393ee559c3a0628
Andrés Gasson <gas@red-elvis.net>
2019-12-11 13:55:12 +1300

    added govc bash setup

--- source/50_net.sh
+++ source/50_net.sh
@@ -19,6 +19,9 @@ function pingtest() {
 
 if which govc  >/dev/null 2>&1
 then
+    export GOVC_URL=https://administrator@vsphere.local:${VCENTER_PASS}@vcenter.ateasystems.com
+    export GOVC_TLS_KNOWN_HOSTS=~/.govc_known_hosts
+    export GOVC_INSECURE=true
     function vm.ip() {
         govc vm.info -json $1 | jq -r .VirtualMachines[].Guest.Net[].IpAddress[0]
     }
@@ -35,4 +38,52 @@ fi
 if which curl >/dev/null 2>&1
 then
     alias curl-trace='curl -w "@$HOME/.curl-format" -o /dev/null -s'
+fi
+
+## gpg-agent
+
+if command -v gpg-connect-agent >/dev/null 2>&1; then
+
+    gpg_restart() {
+        gpg-connect-agent reloadagent /bye
+    }
+
+    gpg_forget() {
+        gpg-connect-agent reloadagent /bye
+    }
+
+fi
+
+## ssh-agent
+
+if command -v ssh-agent >/dev/null 2>&1; then
+
+    ssh_connect() {
+        if [ -n "$HOME" ] && [ -f "$HOME/.ssh/agent-info" ]; then
+            eval "$(cat "$HOME/.ssh/agent-info")" >/dev/null
+        fi
+    }
+
+    ssh_connected() {
+        ps -p "$SSH_AGENT_PID" 2>&1 | grep -qF ssh-agent
+    }
+
+    ssh_forget() {
+        ssh-add -D
+    }
+
+    ssh_restart() {
+        if [ -n "$HOME" ]; then
+            pkill -U "$USER" ssh-agent
+            mkdir -p "$HOME/.ssh"
+            ssh-agent -t 86400 > "$HOME/.ssh/agent-info"
+            ssh_connect
+        fi
+    }
+
+    ssh_connect
+    if ! ssh_connected; then
+        ssh_restart
+    fi
+
 fi
