1b5370096dd2e9a375cf3aa6f277f3819a3bf111
Andrés Gasson <gas@red-elvis.net>
2020-03-27 16:23:45 +1300

    fixed non-zero variables

--- source/10_tmux.sh
+++ source/10_tmux.sh
@@ -3,21 +3,21 @@
 function tm() {
   local is_source=; [[ "$1" == "SOURCE" ]] && is_source=1 && shift
   local tmux_no_logout=~/$DOTFILES/caches/tmux-no-logout
-  if [[ ! "$TMUX" ]]; then
+  if [[ ! -z "${TMUX:-}" ]]; then
     # Clean up any orphaned "no logout" file.
     [[ -e $tmux_no_logout ]] && rm $tmux_no_logout
     # Actually start tmux.
     tmux ls >/dev/null 2>&1 && tmux attach "$@" || tmux "$@"
     # If "no logout" doesn't exist, exit.
     [[ -e $tmux_no_logout ]] && rm $tmux_no_logout || exit
-  elif [[ ! "$is_source" ]]; then
+  elif [[ ! -z "${is_source:-}" ]]; then
     echo "Déjà dans une session tmux!"
   fi
 }
 
 # Start tmux now (at login), but only if in a login shell and not already
 # started (and possibly detached) in this shell.
-if shopt -q login_shell && [[ ! "$TMUX_AUTO_STARTED" ]]; then
+if shopt -q login_shell && [[ ! -z "${TMUX_AUTO_STARTED:-}" ]]; then
   TMUX_AUTO_STARTED=1
   # tm SOURCE
 fi
