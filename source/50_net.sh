8d07629c188454ee6c2e4ddc1dce9ba913815561
Andrés Gasson <gas@red-elvis.net>
2020-04-02 08:25:06 +1300

    added default VCENTER_PASS

--- source/50_net.sh
+++ source/50_net.sh
@@ -19,14 +19,15 @@ function pingtest() {
 
 if which govc  >/dev/null 2>&1
 then
-    export GOVC_URL=https://administrator@vsphere.local:${VCENTER_PASS}@vcenter.ateasystems.com
+    export VCENTER_PASS="${VCENTER_PASS:-changeme}"
+    export GOVC_URL=https://administrator@vsphere.local:${VCENTER_PASS}@vcenter.atea.dev
     export GOVC_TLS_KNOWN_HOSTS=~/.govc_known_hosts
     export GOVC_INSECURE=true
     function vm.ip() {
         govc vm.info -json $1 | jq -r .VirtualMachines[].Guest.Net[].IpAddress[0]
     }
-    alias vm.poweron='govc vm.power -on=true ';
-    alias vm.poweroff='govc vm.power -off=true';
+    alias vm.power.on='govc vm.power -on=true ';
+    alias vm.power.off='govc vm.power -off=true';
     alias vm.dev.ls='govc device.ls -vm ';
 fi
 
@@ -43,7 +44,7 @@ fi
 getIP()  {
   curr_hostname=$(hostname -s)
   interface=${1:-eth0}
-  perldoc -l Regexp::Common >/dev/null 2>&1 
+  perldoc -l Regexp::Common >/dev/null 2>&1
   if [ $? -eq 0  ]  ; then
         local ip=$(ifconfig $interface | perl -MRegexp::Common -lne 'print $1 if /($RE{net}{IPv4})/' | grep -v "127.0.0.1")
     else
