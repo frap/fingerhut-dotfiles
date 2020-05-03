ceb91ae157a93c1da4632c17a3d8775f0522c95b
Andrés Gasson <gas@red-elvis.net>
2020-05-03 16:53:55 +1200

    adde doom bin path

--- source/80_atea_paths.sh
+++ source/80_atea_paths.sh
@@ -3,4 +3,6 @@ prepend_to_path_if_exists "/home/atearoot/.sqlscripts"
 prepend_to_path_if_exists "/home/atearoot/Dev/scripts"
 #-------- Check if rust installed -------
 prepend_to_path_if_exists "$HOME/.cargo/bin"
+#-- if doom emacs add bin/doom  ---------
+prepend_to_path_if_exists "$HOME/.emacs.d/bin"
 
