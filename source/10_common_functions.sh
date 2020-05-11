82f0e163d9baef9ee4dc4cd122470e0d5b67b84a
Andrés Gasson <gas@red-elvis.net>
2020-05-11 13:51:40 +1200

    removed extra config from common_fucntions and moved colours into dotfiles

--- source/10_common_functions.sh
+++ source/10_common_functions.sh
@@ -1,97 +1,3 @@
-if tput setaf 1 &> /dev/null; then
-	tput sgr0; # reset colors
-	_bold=$(tput bold);
-	_reset=$(tput sgr0);
-	# Solarized colors, taken from http://git.io/solarized-colors.
-  _uline=$(tput setaf 4);
-  _inverse=$(tput setaf 7);
-	_black=$(tput setaf 0);
-	_blue=$(tput setaf 33);
-	_cyan=$(tput setaf 37);
-	_green=$(tput setaf 64);
-	_orange=$(tput setaf 166);
-	_purple=$(tput setaf 125);
-	_red=$(tput setaf 124);
-	_violet=$(tput setaf 61);
-	_white=$(tput setaf 15);
-	_yellow=$(tput setaf 136);
-else
-  _bold='';
-  _reset="\e[0m";
-  _uline="\e[4m";
-  _inverse="\e[7m";
-  _black="\e[1;30m";
-  _blue="\e[1;34m";
-  _cyan="\e[1;36m";
-  _green="\e[1;32m";
-  _orange="\e[1;33m";
-  _purple="\e[1;35m";
-  _red="\e[1;31m";
-  _violet="\e[1;35m";
-  _white="\e[1;37m";
-  _yellow="\e[1;33m";
-fi;
- # Logging stuff.
- function e_header()   { echo -e "\n${_bold}${_purple}$@${_reset}"; }
- function e_success()  { echo -e " ${_bold}${_cyan}✔ $@${_reset}"; }
- function e_error()    { echo -e " ${_bold}${_red}✖  $@${_reset}"; }
- function e_arrow()    { echo -e " ${_bold}${_yellow}➜${_reset}${_cyan} $@${_reset}"; }
- function e_info()     { echo -e " ${_green}∴ $@${_reset}"; }
- function e_data()     { echo -e " ${_green}$@${_reset}"; }
- function e_line()     { echo -e " ${_orange}$@${_reset}"; }
- function e_question() { echo -e " ${_violet}$@${_reset}"; }
-# function flasher ()   { while true; do printf \\e[?5h; sleep 0.1; printf \\e[?5l; read -s -n1 -t1 && break; done; }
-
-is_empty() {
-  local var=$1
-
-  [[ -z $var ]]
-}
-
-is_not_empty() {
-  local var=$1
-
-  [[ -n $var ]]
-}
-
-is_file() {
-  local file=$1
-
-  [[ -f $file ]]
-}
-
-file_exists() {
-  local file=$1
-
-  sudo test -e $1
-}
-
-
-is_dir() {
-  local dir=$1
-
-  [[ -d $dir ]]
-}
-
-is_not_dir() {
-  local dir=$1
-
-  [[ ! -d $dir ]]
-}
-
-is_link() {
-  local dir=$1
-
-  [[ -L $dir ]]
-}
-
-matches_regex() {
-  local filepath=$1
-  local regex=$2
-
-  [[ $filepath =~ $regex ]]
-}
-
 # For testing.
 # function assert() {
 #   local success modes equals actual expected
@@ -134,6 +40,7 @@ trap ctrl_c INT
 function ctrl_c() {
   e_error "Trapped CTRL-C - exiting"
 }
+
 prepend_to_path_if_exists () {
     local dir=$1
     if [ -d "${dir}" ]
--- source/60_app.sh
+++ source/60_app.sh
@@ -1,68 +1,31 @@
-# source secrets such as VCENTER_PASS
-if [ -f ~/.secrets ]; then
-    . ~/.secrets
-fi
-
-if has_gls
-then
-    alias ls="gls --color"
-    alias ll="gls --color -alhFtr"
-    alias  l="gls --color -alhF"
-    alias lls="gls --color -alhSr"
-    alias la="gls --color -Atr"
-    alias dir="gls --color=auto"
+if has_rg; then
+	alias grep='rg'
 else
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    alias ll='ls -alhFtr'
-    alias  l='ls -alhF'
-    alias lls='ls -alhSr'
-    alias la='ls -Atr'
-    alias dir='dir --color=auto'
-    alias vdir='vdir --color=auto'
+	alias grep='grep --color=auto'
+	alias fgrep='fgrep --color=auto'
+	alias egrep='egrep --color=auto'
 fi
 
-alias h="history"
-alias clr="clear" # Clear your terminal screen
-alias myip="curl icanhazip.com" # Your public IP address
-alias path='echo -e ${PATH//:/\\n}'
-alias now='date +"%T"'
-alias curdate='date +"%d-%m-%Y"'
-
-if has_rg
-then
-    alias grep='rg'
-else
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
+if is_osx; then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh" # This loads nvm
+	[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
 fi
 
-if is_osx
-then
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
+if is_redhat && [[ -r /atea/home/atearoot ]]; then
+	export NVM_DIR="/atea/home/atearoot/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 fi
 
-if is_redhat && [[ -r /atea/home/atearoot ]]
-then
-    export NVM_DIR="/atea/home/atearoot/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+if has_gpg_agent; then
+	gpg_restart() {
+		gpg-connect-agent reloadagent /bye
+	}
+
+	gpg_forget() {
+		gpg-connect-agent reloadagent /bye
+	}
 fi
-
-
-if has_gpg_agent
-then
-     gpg_restart() {
-        gpg-connect-agent reloadagent /bye
-    }
-
-    gpg_forget() {
-        gpg-connect-agent reloadagent /bye
-    }
-fi
-
 
 #if has_ssh_agent
 #then
@@ -75,36 +38,35 @@ fi
 #        ps -p "$SSH_AGENT_PID" 2>&1 | grep -qF ssh-agent
 #    }
 
- #   ssh_forget() {
- #       ssh-add -D
- #   }
+#   ssh_forget() {
+#       ssh-add -D
+#   }
 
- #   ssh_restart() {
- #       if [ -n "$HOME" ]; then
- #           pkill -U "$USER" ssh-agent
- #           mkdir -p "$HOME/.ssh"
- #           ssh-agent -t 86400 > "$HOME/.ssh/agent-info"
- #           ssh_connect
- #       fi
- #   }
+#   ssh_restart() {
+#       if [ -n "$HOME" ]; then
+#           pkill -U "$USER" ssh-agent
+#           mkdir -p "$HOME/.ssh"
+#           ssh-agent -t 86400 > "$HOME/.ssh/agent-info"
+#           ssh_connect
+#       fi
+#   }
 
- #   ssh_connect
- #   if ! ssh_connected; then
- #       ssh_restart
- #   fi
+#   ssh_connect
+#   if ! ssh_connected; then
+#       ssh_restart
+#   fi
 #fi
 
-if has_docker
-then
-    # ---------------------------------
-    # Docker
-    # ---------------------------------
-    alias macaddress="docker inspect --format '{{ range .NetworkSettings.Networks}}{{.MacAddress }}{{end}}'"
-    alias ipaddress="docker inspect --format '{{ range .NetworkSettings.Networks}}{{.IPAddress }}{{end}}'"
-    alias logpath="docker inspect --format='{{.LogPath}}'"
-    alias dsl='docker stack ls'
-    alias dss='docker stack services'
-    alias dsp='docker stack ps'
+if has_docker; then
+	# ---------------------------------
+	# Docker
+	# ---------------------------------
+	alias macaddress="docker inspect --format '{{ range .NetworkSettings.Networks}}{{.MacAddress }}{{end}}'"
+	alias ipaddress="docker inspect --format '{{ range .NetworkSettings.Networks}}{{.IPAddress }}{{end}}'"
+	alias logpath="docker inspect --format='{{.LogPath}}'"
+	alias dsl='docker stack ls'
+	alias dss='docker stack services'
+	alias dsp='docker stack ps'
 fi
 
 # ---------------------------------
