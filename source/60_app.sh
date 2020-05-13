e82a5ef19b4530defe02ce59b4e739ce91993018
Andrés Gasson <gas@red-elvis.net>
2020-05-14 11:30:12 +1200

    merging and adding bash-it ssh functions * 'master' of https://github.com/frap/atea-dotfiles:

--- source/60_app.sh
+++ source/60_app.sh
@@ -167,6 +167,23 @@ then
 
     sshagent on
 
+    function add_ssh() {
+
+      [[ $# -ne 3 ]] && echo "add_ssh host hostname user" && return 1
+      [[ ! -d ~/.ssh ]] && mkdir -m 700 ~/.ssh
+      [[ ! -e ~/.ssh/config ]] && touch ~/.ssh/config && chmod 600 ~/.ssh/config
+      echo -en "\n\nHost $1\n  HostName $2\n  User $3\n  ServerAliveInterval 30\n  ServerAliveCountMax 120" >> ~/.ssh/config
+    }
+
+    function sshlist() {
+      awk '$1 ~ /Host$/ {for (i=2; i<=NF; i++) print $i}' ~/.ssh/config
+    }
+
+    function ssh-add-all() {
+      grep -slR "PRIVATE" ~/.ssh | xargs ssh-add
+    }
+
+
 fi
 
 if has_docker; then
