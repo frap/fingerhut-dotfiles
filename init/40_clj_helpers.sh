67157d6fed3dde2c4463534701c7ab3906cd25d8
Andrés Gasson <gas@red-elvis.net>
2020-06-15 12:31:26 +1200

    added jet as well as echo_reset_color

--- init/40_clj_helpers.sh
+++ init/40_clj_helpers.sh
@@ -1,3 +1,6 @@
 # Install babashka a clojure scripting env
 e_install "Installing babashka - a graalvm based clj"
 sudo su -c 'bash <(curl -s https://raw.githubusercontent.com/borkdude/babashka/master/install)'
+
+e_install "Installing jet - json to edn converter"
+sudo su -c 'bash <(curl -s https://raw.githubusercontent.com/borkdude/jet/master/install)'
