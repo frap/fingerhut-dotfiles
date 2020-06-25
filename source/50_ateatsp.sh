f83262bf83bd6e4bb30af0189c530093ee50c0b1
Andrés Gasson <gas@red-elvis.net>
2020-06-25 14:21:39 +1200

    added -o cat to tt

--- source/50_ateatsp.sh
+++ source/50_ateatsp.sh
@@ -8,7 +8,7 @@ function has_ccze() {
 if has_tomcat && has_ccze
 then
     alias ccze='ccze -A -o nolookups'
-    alias tt='jctl -u tomcat -f | ccze -A -o nolookups'
+    alias tt='jctl -u tomcat -o cat -f | ccze -A -o nolookups'
     alias tomcattail=tt
     alias tt2day='jctl -u tomcat -o cat -S today | ccze'
     alias tt2min='jctl -u tomcat -o cat -S "2 minutes ago" | ccze'
