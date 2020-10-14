3be38cb9d9932052273443445fcf7a5d71186378
Andrés Gasson <gas@red-elvis.net>
2020-10-15 12:08:22 +1300

    commented out stuff not using

--- source/30_osx.sh
+++ source/30_osx.sh
@@ -72,56 +72,56 @@ function pwcopy {
     && echo
   }
 # Create a new Parallels VM from template, replacing the existing one.
-function vm_template() {
-  local name="$@"
-  local basename="$(basename "$name" ".zip")"
-  local dest_dir="$HOME/Documents/Parallels"
-  local dest="$dest_dir/$basename"
-  local src_dir="$dest_dir/Templates"
-  local src="$src_dir/$name"
-  if [[ ! "$name" || ! -e "$src" ]]; then
-    echo "You must specify a valid VM template from this list:";
-    shopt -s nullglob
-    for f in "$src_dir"/*.pvm "$src_dir"/*.pvm.zip; do
-      echo " * $(basename "$f")"
-    done
-    shopt -u nullglob
-    return 1
-  fi
-  if [[ -e "$dest" ]]; then
-    echo "Deleting old VM"
-    rm -rf "$dest"
-  fi
-  echo "Restoring VM template"
-  if [[ "$name" == "$basename" ]]; then
-    cp -R "$src" "$dest"
-  else
-    unzip -q "$src" -d "$dest_dir" && rm -rf "$dest_dir/__MACOSX"
-  fi && \
-  echo "Starting VM" && \
-  open -g "$dest"
-}
+# function vm_template() {
+#   local name="$@"
+#   local basename="$(basename "$name" ".zip")"
+#   local dest_dir="$HOME/Documents/Parallels"
+#   local dest="$dest_dir/$basename"
+#   local src_dir="$dest_dir/Templates"
+#   local src="$src_dir/$name"
+#   if [[ ! "$name" || ! -e "$src" ]]; then
+#     echo "You must specify a valid VM template from this list:";
+#     shopt -s nullglob
+#     for f in "$src_dir"/*.pvm "$src_dir"/*.pvm.zip; do
+#       echo " * $(basename "$f")"
+#     done
+#     shopt -u nullglob
+#     return 1
+#   fi
+#   if [[ -e "$dest" ]]; then
+#     echo "Deleting old VM"
+#     rm -rf "$dest"
+#   fi
+#   echo "Restoring VM template"
+#   if [[ "$name" == "$basename" ]]; then
+#     cp -R "$src" "$dest"
+#   else
+#     unzip -q "$src" -d "$dest_dir" && rm -rf "$dest_dir/__MACOSX"
+#   fi && \
+#   echo "Starting VM" && \
+#   open -g "$dest"
+# }
 
 # Export Localisation.prefPane text substitution rules.
-function txt_sub_backup() {
-  local prefs=~/Library/Preferences/.GlobalPreferences.plist
-  local backup=$DOTFILES/conf/osx/NSUserReplacementItems.plist
-  /usr/libexec/PlistBuddy -x -c "Print NSUserReplacementItems" "$prefs" > "$backup" &&
-  echo "Le fichier ~${backup#$HOME} écrire."
-}
+# function txt_sub_backup() {
+#   local prefs=~/Library/Preferences/.GlobalPreferences.plist
+#   local backup=$DOTFILES/conf/osx/NSUserReplacementItems.plist
+#   /usr/libexec/PlistBuddy -x -c "Print NSUserReplacementItems" "$prefs" > "$backup" &&
+#   echo "Le fichier ~${backup#$HOME} écrire."
+# }
 
 # Import Localization.prefPane text substitution rules.
-function txt_sub_restore() {
-  local prefs=~/Library/Preferences/.GlobalPreferences.plist
-  local backup=$DOTFILES/conf/osx/NSUserReplacementItems.plist
-  if [[ ! -e "$backup" ]]; then
-    echo "Erreur: le fichier  ~${backup#$HOME} n'existe pas!"
-    return 1
-  fi
-  cmds=(
-    "Delete NSUserReplacementItems"
-    "Add NSUserReplacementItems array"
-    "Merge '$backup' NSUserReplacementItems"
-  )
-  for cmd in "${cmds[@]}"; do /usr/libexec/PlistBuddy -c "$cmd" "$prefs"; done
-}
+# function txt_sub_restore() {
+#   local prefs=~/Library/Preferences/.GlobalPreferences.plist
+#   local backup=$DOTFILES/conf/osx/NSUserReplacementItems.plist
+#   if [[ ! -e "$backup" ]]; then
+#     echo "Erreur: le fichier  ~${backup#$HOME} n'existe pas!"
+#     return 1
+#   fi
+#   cmds=(
+#     "Delete NSUserReplacementItems"
+#     "Add NSUserReplacementItems array"
+#     "Merge '$backup' NSUserReplacementItems"
+#   )
+#   for cmd in "${cmds[@]}"; do /usr/libexec/PlistBuddy -c "$cmd" "$prefs"; done
+# }
