3bf6a5f7c257456518d90151238b406f966f3bc9
Andrés Gasson <gas@red-elvis.net>
2020-10-15 12:05:02 +1300

    added has_nvm b4 install

--- init/30_node.sh
+++ init/30_node.sh
@@ -2,5 +2,7 @@
 is_linux || return 1
 
 # Install nvm
-e_install "Installing nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+if ! has_nvm
+   e_install "Installing nvm"
+   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+fi
